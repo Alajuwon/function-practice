1310e440bdee7f93960fcf8b47c82847d932657d
Alajuwon Thomas <spooda7@yahoo.com>
2016-10-07 06:54:38 -0400

    added array file

---
+++ arrays.js
@@ -0,0 +1,113 @@
+// ---------------------------
+// The Arrays You Will Work With
+// ---------------------------
+var numbers = [1,12,4,18,9,7,11,3,101,5,6];
+var strings = ['this','is','a','collection','of','words'];
+var instructors = [
+    { firstname : 'JD', teaches : 'JavaScript'},
+    { firstname : 'Tim', teaches : 'JavaScript'},
+    { firstname : 'Brit', teaches : 'Ruby'},
+    { firstname : 'Joe', teaches : 'iOS'},
+    { firstname : 'Jake', teaches : 'JavaScript'},
+    { firstname : 'Will', teaches : 'JavaScript'},
+    { firstname : 'Calvin', teaches : 'JavaScript'},
+    { firstname : 'James', teaches : 'Ruby'}
+];
+
+
+
+// ---------------------------
+// 1. Find largest number
+// ---------------------------
+var numbers = [1,12,4,18,9,7,11,3,101,5,6];
+
+function findMax (number) {
+	var number = 0;
+
+	for (var count = 0; count < numbers.length; count++){
+		var number = number[count];
+		
+		if (number > largest) {
+			largest = number;
+		}
+	}
+return largest;
+}
+// ---------------------------
+// 2. Find longest string
+// ---------------------------
+var strings = ['this','is','a','collection','of','words'];
+
+function longeststring(longest){
+var longeststring = 0;
+
+for (var count = 0; count < longeststring.length; count++){
+	var longest = 0; number[count];
+
+		if (longest > largest){
+			(largest = number)
+		}
+	}
+return longest;
+}
+
+
+// ---------------------------
+// 3. Find even numbers
+// ---------------------------
+var numbers = [1,12,4,18,9,7,11,3,101,5,6];
+
+	function findeven (even){
+ 		var even = {}
+
+		for (var count = 0; count < findeven.length; count++){
+			if(number[count] %2===0 numbers.length){
+				even.push( numbers === 1);
+					even = numbers 
+			}
+	}
+	return even 
+}
+
+
+// ---------------------------
+// 4. Find odd numbers
+// ---------------------------
+var numbers = [1,12,4,18,9,7,11,3,101,5,6];
+
+function findodd (odd){
+	var odd = {}
+	for (var count = 0; count < numbers.length; count++){
+			if(number[count]%2===1){
+				odd.push(numbers===1);
+					odd = numbers
+			}
+		}
+	return odd
+}
+
+// ---------------------------
+// 5. Find words that contain `is`
+// ---------------------------
+var strings = ['this','is','a','collection','of','words'];
+
+function findword (string){
+	var collection = 0
+	for (var count = 0; count>words.length; count++){
+		if(word[count] )
+
+	}
+}
+
+
+// ---------------------------
+// 5. Join Both Arrays Together
+// ---------------------------
+
+
+
+
+// ---------------------------
+// 6. Use the Instructors array and find all that teach JavaScript, 
+//    then sort them alphabetically
+// ---------------------------
